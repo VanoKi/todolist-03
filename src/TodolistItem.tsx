90b958d9ac72ff08c069b8c680d3e9e5eef850d7
VanoKi <kirsvano@gmail.com>
2025-08-12 14:48:06 +0300

    feat: add map to todolistItm

--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -1,21 +1,37 @@
-export const TodolistItem = () => {
+import {Task} from "./App.tsx";
+
+type todolistitemProps = {
+  title: string
+  tasks: Task[]
+}
+
+export const TodolistItem = (props: todolistitemProps) => {
+  const {title, tasks} = props
   return (
     <div>
-      <h3>What to learn</h3>
+      <h3>{title}</h3>
       <div>
         <input/>
         <button>+</button>
       </div>
       <ul>
-        <li>
-          <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
-        </li>
-        <li>
-          <input type="checkbox" checked={true}/> <span>JS</span>
-        </li>
-        <li>
-          <input type="checkbox" checked={false}/> <span>React</span>
-        </li>
+        {tasks.map((task) => {
+          return (
+            <li key={task.id}>
+              <input type={"checkbox"} checked={task.isDone}/>
+              <span>{task.title}</span>
+            </li>
+          )
+        })}
+        {/*<li>*/}
+        {/*  <input type="checkbox" checked={true}/> <span>HTML&CSS</span>*/}
+        {/*</li>*/}
+        {/*<li>*/}
+        {/*  <input type="checkbox" checked={true}/> <span>JS</span>*/}
+        {/*</li>*/}
+        {/*<li>*/}
+        {/*  <input type="checkbox" checked={false}/> <span>React</span>*/}
+        {/*</li>*/}
       </ul>
       <div>
         <button>All</button>
